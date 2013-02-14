6110ec63c5ce0de99845fc678b9726c8b1ff10a2
Duane Costa <dcosta@lternet.edu>
2013-02-14 16:14:07 -0700

    Add curl test script.

---
+++ AuditManager/bin/curl_query.bat
@@ -0,0 +1,6 @@
+@ECHO OFF
+SET SERVICE_HOST=http://localhost:8080
+SET AUTH_TOKEN_UCARROLL=dWlkPXVjYXJyb2xsLG89TFRFUixkYz1lY29pbmZvcm1hdGljcyxkYz1vcmcqaHR0cHM6Ly9wYXN0YS5sdGVybmV0LmVkdS9hdXRoZW50aWNhdGlvbioyMDAwMDAwMDAwKmF1dGhlbnRpY2F0ZWQ=
+SET AUTH_TOKEN_DCOSTA=dWlkPWRjb3N0YSxvPUxURVIsZGM9ZWNvaW5mb3JtYXRpY3MsZGM9b3JnKmh0dHBzOi8vcGFzdGEubHRlcm5ldC5lZHUvYXV0aGVudGljYXRpb24qMjAwMDAwMDAwMCphdXRoZW50aWNhdGVk
+
+curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G "%SERVICE_HOST%/audit/report?category=warn&user=uid=dcosta,o=LTER,dc=ecoinformatics,dc=org&responseStatus=400&fromtime=2013-01-01&totime=2013-01-20"
