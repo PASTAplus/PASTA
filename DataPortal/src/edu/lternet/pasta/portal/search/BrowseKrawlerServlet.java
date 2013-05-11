590b1671d93e07584331d863b96af3aa398a89b9
Mark Servilla <servilla@lternet.edu>
2013-05-10 22:24:24 -0600

    BrowseKrawlerServlet.java
    Remove import for javax.servlet.annotation.WebServlet.

--- DataPortal/src/edu/lternet/pasta/portal/search/BrowseKrawlerServlet.java
+++ DataPortal/src/edu/lternet/pasta/portal/search/BrowseKrawlerServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -53,14 +52,14 @@ public class BrowseKrawlerServlet extends HttpServlet {
 	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 *
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		this.doPost(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * 
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
