35310bad28bd81db7f0bada862b417892c1a8c76
Mark Servilla <servilla@lternet.edu>
2013-05-10 22:11:56 -0600

    BrowseKrawlerServlet.java
    Add new servlet for initiating browse crawl from URL.

---
+++ DataPortal/src/edu/lternet/pasta/portal/search/BrowseKrawlerServlet.java
@@ -0,0 +1,80 @@
+package edu.lternet.pasta.portal.search;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.apache.commons.configuration.PropertiesConfiguration;
+
+import edu.lternet.pasta.portal.ConfigurationListener;
+
+/**
+ * Servlet implementation class BrowseKrawlerServlet
+ */
+public class BrowseKrawlerServlet extends HttpServlet {
+	
+	/*
+	 * Class variables
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	/*
+	 * Instance variables
+	 */
+	
+	private String browseDir = null;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BrowseKrawlerServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		PropertiesConfiguration options = ConfigurationListener.getOptions();
+		browseDir = options.getString("browse.dir");
+
+		if ((browseDir == null) || (browseDir.equals(""))) {
+			throw new ServletException(
+			    "No value defined for 'browse.dir' property.");
+		}
+		
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		this.doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+    BrowseSearch.setBrowseCacheDir(browseDir);
+    
+    BrowseCrawler browseCrawler;
+    
+    browseCrawler = new BrowseCrawler();
+    BrowseGroup browseCache = browseCrawler.crawl();
+    String html = browseCache.toHTML();
+    HttpSession httpSession = request.getSession();
+    ServletContext servletContext = httpSession.getServletContext();
+    servletContext.setAttribute("browseHTML", html);
+    
+	}
+
+}
