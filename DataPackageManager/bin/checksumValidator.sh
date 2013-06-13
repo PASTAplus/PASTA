d33120be00d1343ac93112c6ce08652a036532d5
Duane Costa <dcosta@lternet.edu>
2013-06-13 14:02:54 -0700

    Direct checksum validator output to a timestamped filename.

--- DataPackageManager/bin/checksumValidator.sh
+++ DataPackageManager/bin/checksumValidator.sh
@@ -1,1 +1,4 @@
-java -cp $DATAPACKAGEMANAGER/WebRoot/WEB-INF/classes:$DATAPACKAGEMANAGER/WebRoot/WEB-INF/lib/*:$TOMCAT/lib/servlet-api.jar edu.lternet.pasta.datapackagemanager.checksum.ChecksumValidator
+CHECKSUM_DIR=/home/pasta/local/checksum
+mkdir -p $CHECKSUM_DIR
+DATE=$(date +"%Y-%m-%d_%H:%M:%S")
+java -cp $DATAPACKAGEMANAGER/WebRoot/WEB-INF/classes:$DATAPACKAGEMANAGER/WebRoot/WEB-INF/lib/*:$TOMCAT/lib/servlet-api.jar edu.lternet.pasta.datapackagemanager.checksum.ChecksumValidator >& $CHECKSUM_DIR/$DATE
