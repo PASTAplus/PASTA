9635fe5ddbaed7e8afa38a50399c7e8ace5eed09
Mark Servilla <servilla@lternet.edu>
2013-08-15 10:14:29 -0600

    DatabaseManager.java, FullPublic.java, StatReporter.java
    Commit initial utility classes.

---
+++ utilities/src/edu/lternet/pasta/utilities/statistics/FullPublic.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2011-2013 the University of New Mexico.
+ *
+ * This work was supported by National Science Foundation Cooperative
+ * Agreements #DEB-0832652 and #DEB-0936498.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+package edu.lternet.pasta.utilities.statistics;
+
+/**
+ * User: servilla
+ * Date: 8/13/13
+ * Time: 7:48 PM
+ * <p/>
+ * Project: Utilities
+ * Package: edu.lternet.pasta.utilities.statistics
+ * <p/>
+ * Calculate statistics for data packages that are fully public - that is, all
+ * resource objects for the data package are publicly accessible.
+ */
+public class FullPublic {
+
+  /* Constructors */
+
+  /* Instance methods */
+
+  /* Class methods */
+
+  public static void main(String[] args) {
+
+  }
+
+  /*  Instance variables */
+
+  // Name of the database table where data packages are registered
+  private final String ACCESS_MATRIX_TABLE = "ACCESS_MATRIX";
+  private final String ACCESS_MATRIX = "datapackagemanager.ACCESS_MATRIX";
+  private final String DATA_PACKAGE_MANAGER_SCHEMA = "datapackagemanager";
+  private final String RESOURCE_REGISTRY = "datapackagemanager.RESOURCE_REGISTRY";
+  private final String RESOURCE_REGISTRY_TABLE = "RESOURCE_REGISTRY";
+
+
+  /* Class variables */
+
+}
