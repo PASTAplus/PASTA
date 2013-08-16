76cde5f7d429e83c5e033cde503f96cff01d2299
Mark Servilla <servilla@lternet.edu>
2013-08-16 13:45:19 -0600

    FullPublic.java
    Add getPackageIds method.

--- utilities/src/edu/lternet/pasta/utilities/statistics/FullPublic.java
+++ utilities/src/edu/lternet/pasta/utilities/statistics/FullPublic.java
@@ -18,6 +18,10 @@
 
 package edu.lternet.pasta.utilities.statistics;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 /**
  * User: servilla
  * Date: 8/13/13
@@ -33,15 +37,68 @@ public class FullPublic {
 
   /* Constructors */
 
+  public FullPublic(String dbUrl, String dbUser, String dbPassword) {
+
+    try {
+      this.dbm = new DatabaseManager(dbUrl, dbUser, dbPassword);
+    }
+    catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+
+  }
+
   /* Instance methods */
+
+  private ArrayList<String> getPackageIds() {
+
+    String sql = "SELECT distinct(package_id) from "
+      + "datapackagemanager.resource_registry";
+
+    ArrayList<String[]> result = null;
+
+    try {
+      ResultSet rs = dbm.doQuery(sql);
+      result = dbm.resultSetAsString(rs);
+    }
+    catch (SQLException e) {
+      e.printStackTrace();
+    }
+
+    ArrayList<String> packageIds = null;
+    for (String[] tuple: result) {
+      packageIds.add(tuple[0]);
+    }
+
+    return packageIds;
+
+  }
 
   /* Class methods */
 
   public static void main(String[] args) {
 
+    String dbUrl = args[0];
+    String dbUser = args[1];
+    String dbPassword = args[2];
+
+    FullPublic fp = new FullPublic(dbUrl, dbUser, dbPassword);
+    ArrayList<String> packageIds = fp.getPackageIds();
+
+    for (String packageId: packageIds) {
+      System.out.printf("%s\n", packageId);
+    }
+
   }
 
   /*  Instance variables */
+
+  DatabaseManager dbm = null;
+
+  private String dbUrl = null;
+  private String dbUser = null;
+  private String dbPassword = null;
 
   // Name of the database table where data packages are registered
   private final String ACCESS_MATRIX_TABLE = "ACCESS_MATRIX";
@@ -49,7 +106,6 @@ public class FullPublic {
   private final String DATA_PACKAGE_MANAGER_SCHEMA = "datapackagemanager";
   private final String RESOURCE_REGISTRY = "datapackagemanager.RESOURCE_REGISTRY";
   private final String RESOURCE_REGISTRY_TABLE = "RESOURCE_REGISTRY";
-
 
   /* Class variables */
 
