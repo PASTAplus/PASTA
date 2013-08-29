5be68ea4534140d64ffae5ddd0878330492c00a9
Mark Servilla <servilla@lternet.edu>
2013-08-29 13:58:01 -0600

    GrowthStats.java
    Add methods to create site growth statisitics.

--- utilities/src/edu/lternet/pasta/utilities/statistics/GrowthStats.java
+++ utilities/src/edu/lternet/pasta/utilities/statistics/GrowthStats.java
@@ -56,11 +56,11 @@ public class GrowthStats {
 
  /* Instance methods */
 
-  private HashMap<String, Integer> buildPackageHashMap(int year) {
+  private HashMap<String, Integer> buildPackageHashMapForYear(int year) {
 
     HashMap<String, Integer> map = new HashMap<String, Integer>();
 
-    for (Integer week = 1; week <= YEARWEEKS; week++) {
+    for (Integer week = 1; week <= WEEKSINYEAR; week++) {
       for (String pkg: dataPackagesForWeek(year, week)) {
         if (!map.containsKey(pkg)) {
           map.put(pkg, week);
@@ -72,12 +72,28 @@ public class GrowthStats {
 
   }
 
+  private HashMap<String, Integer> buildSiteHashMapForYear(int year) {
+
+    HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+    for (Integer week = 1; week <= WEEKSINYEAR; week++) {
+      for (String site: sitesForWeek(year, week)) {
+        if (!map.containsKey(site)) {
+          map.put(site, week);
+        }
+      }
+    }
+
+    return map;
+
+  }
+
   private int[] buildPackageStats(HashMap<String, Integer> packageHashMap) {
 
-    int[] packageStats = new int[52];
+    int[] packageStats = new int[WEEKSINYEAR];
 
     // Initialize each cell to 0
-    for (int i = 0; i < YEARWEEKS; i++) {
+    for (int i = 0; i < WEEKSINYEAR; i++) {
       packageStats[i] = 0;
     }
 
@@ -91,12 +107,13 @@ public class GrowthStats {
 
   private ArrayList<String> dataPackagesForWeek(int year, int week) {
 
-    String startDate = this.weekStartToDate(year, week);
-    String endDate = this.weekEndToDate(year, week);
+    String startDate = "'" + this.weekStartToDate(year, week) + " " + STARTHOUR + "'";
+    String endDate = "'" + this.weekEndToDate(year, week) + " " + ENDHOUR + "'";
 
     String sql = "SELECT distinct scope, identifier FROM "
         + RESOURCE_REGISTRY + " WHERE date_created >= " + startDate + " AND "
-        + " date_created <= " + endDate + " AND scope LIKE 'knb-lter-%'";
+        + " date_created <= " + endDate + " AND scope LIKE 'knb-lter-%' "
+        + "AND date_deactivated IS NULL AND NOT scope='knb-lter-nwk'";
 
     ResultSet rs = null;
 
@@ -104,7 +121,7 @@ public class GrowthStats {
       rs = this.dbm.doQuery(sql);
     }
     catch (SQLException e) {
-      System.err.printf("%s\n", e.getMessage());
+      System.err.printf("%s%n", e.getMessage());
       e.printStackTrace();
     }
 
@@ -117,11 +134,47 @@ public class GrowthStats {
 
     }
     catch (SQLException e) {
-      System.err.printf("%s\n", e.getMessage());
+      System.err.printf("%s%n", e.getMessage());
       e.printStackTrace();
     }
 
     return dataPackages;
+
+  }
+
+  private ArrayList<String> sitesForWeek(int year, int week) {
+
+    String startDate = "'" + this.weekStartToDate(year, week) + " " + STARTHOUR + "'";
+    String endDate = "'" + this.weekEndToDate(year, week) + " " + ENDHOUR + "'";
+
+    String sql = "SELECT distinct scope FROM "
+                     + RESOURCE_REGISTRY + " WHERE date_created >= " + startDate + " AND "
+                     + " date_created <= " + endDate + " AND scope LIKE 'knb-lter-%' "
+                     + " AND date_deactivated IS NULL AND NOT scope='knb-lter-nwk'";
+
+    ResultSet rs = null;
+
+    try {
+      rs = this.dbm.doQuery(sql);
+    }
+    catch (SQLException e) {
+      System.err.printf("%s%n", e.getMessage());
+      e.printStackTrace();
+    }
+
+    ArrayList<String> sites = new ArrayList<String>();
+
+    try {
+      for (String[] site: dbm.resultSetAsString(rs)) {
+        sites.add(site[0]);
+      }
+    }
+    catch (SQLException e) {
+      System.err.printf("%s%n", e.getMessage());
+      e.printStackTrace();
+    }
+
+    return sites;
 
   }
 
@@ -137,7 +190,7 @@ public class GrowthStats {
     String monthStr = Integer.toString(now.get(Calendar.MONTH) + 1);
     String dayStr = Integer.toString(now.get(Calendar.DAY_OF_MONTH));
 
-    String date = "'" + yearStr + "-" + monthStr + "-" + dayStr + "'";
+    String date = yearStr + "-" + monthStr + "-" + dayStr;
     return date;
 
   }
@@ -154,7 +207,7 @@ public class GrowthStats {
     String monthStr = Integer.toString(now.get(Calendar.MONTH) + 1);
     String dayStr = Integer.toString(now.get(Calendar.DAY_OF_MONTH));
 
-    String date = "'" + yearStr + "-" + monthStr + "-" + dayStr + "'";
+    String date = yearStr + "-" + monthStr + "-" + dayStr;
     return date;
 
   }
@@ -169,14 +222,18 @@ public class GrowthStats {
 
     GrowthStats gs = new GrowthStats(dbUrl, dbUser, dbPassword);
 
-    HashMap<String, Integer> map = gs.buildPackageHashMap(2013);
-    int[] packageStats = gs.buildPackageStats(map);
+    HashMap<String, Integer> packageMap = gs.buildPackageHashMapForYear(2013);
+    int[] packageStats = gs.buildPackageStats(packageMap);
 
-    int count = 0;
-    System.out.printf("WEEK - COUNT - TOTAL%n");
-    for (int i = 0; i < YEARWEEKS; i++) {
-      count += packageStats[i];
-      System.out.printf(" %d,%d,%d%n", i + 1, packageStats[i], count);
+    HashMap<String, Integer> siteMap = gs.buildSiteHashMapForYear(2013);
+    int[] siteStats = gs.buildPackageStats(siteMap);
+
+    int pkgCount = 0;
+    int siteCount = 0;
+    for (int week = 0; week < WEEKSINYEAR; week++) {
+      pkgCount += packageStats[week];
+      siteCount += siteStats[week];
+      System.out.printf("[%2d, %6d, %2d],%n", week + 1, pkgCount, siteCount);
     }
   }
  
@@ -192,9 +249,11 @@ public class GrowthStats {
 
   private static String RESOURCE_REGISTRY = "datapackagemanager.resource_registry";
   private static int STARTDAY = 1;
+  private static String STARTHOUR = "00:00:00";
   private static int ENDDAY = 7;
+  private static String ENDHOUR = "23:59:59";
   private static int DAYSINWEEK = 7;
-  private static int YEARWEEKS = 52;
+  private static int WEEKSINYEAR = 52;
 
 
 }
