a4d280f761cb4e14155f2af3d70ab2754f8adcaa
Mark Servilla <servilla@lternet.edu>
2014-05-01 16:08:24 -0600

    Update Tomcat shells to reflect current configuration needs.

--- shell/tomcat_startup.sh
+++ shell/tomcat_startup.sh
@@ -22,38 +22,23 @@ export ANT_BINDIR=$ANT_HOME/bin
 # Java variables
 export JAVA_HOME=$APPDIR/java
 export JAVA_BINDIR=$JAVA_HOME/bin
-export JAVA_OPTS="-Xms256M -Xmx1536M -Djava.awt.headless=true -Djavax.xml.transform.TransformerFactory=com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl"
+export JAVA_OPTS="-Xms256M -Xmx1536M -XX:MaxPermSize=512M -Djava.awt.headless=true -Djavax.xml.transform.TransformerFactory=com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl"
 export JAVA_ROOT=$JAVA_HOME
 export JDK_HOME=$JAVA_HOME
 export JRE_HOME=$JAVA_HOME/jre
 export SDK_HOME=$JAVA_HOME
 
-# LTER Subversion repositories and key project directories
-export LNO=$HOME/svn/LNO
-export NIS=$HOME/svn/NIS
-export DATAPORTAL=$LNO/projects/dataPortal
-export LTERHIVE=$NIS/contrib/lter-hive
-export METAMAN=$NIS/software/MetadataManagementSuite/trunk
-
-# Metacat variables
-export METACAT=$APPDIR/metacat-src/metacat
-export METACAT_DATA=$APPDIR/metacat_data
+# LTER Git repository
+export NIS=$HOME/git/NIS
 
 # PATH management
 export PATH=$ANT_BINDIR:$JAVA_BINDIR:$PATH
 
-# Perl variables
-export PERLPATH=$LNO/perl/lib
 
 # Tomcat variables and aliases
 export CATALINA_HOME=$APPDIR/apache-tomcat
 export TOMCAT=$CATALINA_HOME
 export WEBAPPS=$TOMCAT/webapps
-alias tomcat_startup="pushd .;cd $TOMCAT/bin;./startup.sh;popd;tail -f $TOMCAT/logs/catalina.out"
-alias tomcat_startup_notail="pushd .;cd $TOMCAT/bin;./startup.sh;popd"
-alias tomcat_shutdown="$TOMCAT/bin/shutdown.sh; tail -50 $TOMCAT/logs/catalina.out"
-alias tomcat_version="$TOMCAT/bin/version.sh"
-alias tomcat_ps='ps auwwx | grep catalina.startup.Bootstrap' # show Tomcat processes
 
 # Startup Tomcat using the default '$TOMCAT/bin/startup.sh'
 /home/pasta/local/apache-tomcat/bin/startup.sh
