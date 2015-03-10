aa1ed6c87b7aade251a695601394c4788b3a2d54
Duane Costa <dcosta@lternet.edu>
2015-03-10 16:25:36 -0700

    Configuration fix for Audit Manager

--- AuditManager/bin/curl_query.bat
+++ AuditManager/bin/curl_query.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
-SET SERVICE_HOST=http://localhost:8080
-REM SET SERVICE_HOST=http://audit-d.lternet.edu
+REM SET SERVICE_HOST=http://localhost:8080
+SET SERVICE_HOST=http://audit-d.lternet.edu:8080
 SET AUTH_TOKEN_UCARROLL=dWlkPXVjYXJyb2xsLG89TFRFUixkYz1lY29pbmZvcm1hdGljcyxkYz1vcmcqaHR0cHM6Ly9wYXN0YS5sdGVybmV0LmVkdS9hdXRoZW50aWNhdGlvbioyMDAwMDAwMDAwKmF1dGhlbnRpY2F0ZWQ=
 SET AUTH_TOKEN_DCOSTA=dWlkPWRjb3N0YSxvPUxURVIsZGM9ZWNvaW5mb3JtYXRpY3MsZGM9b3JnKmh0dHBzOi8vcGFzdGEubHRlcm5ldC5lZHUvYXV0aGVudGljYXRpb24qMjAwMDAwMDAwMCphdXRoZW50aWNhdGVk
 
@@ -10,5 +10,5 @@ REM curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G "%SERVICE_HOST%/audit/report?
 REM curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G "%SERVICE_HOST%/audit/report?fromTime=2013-12-09&limit=x&serviceMethod=readMetadata"
 REM curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G "%SERVICE_HOST%/audit/report?limit=3&fromTime=2013-12-09"
 REM curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G "%SERVICE_HOST%/audit/report?limit=x&serviceMethod=readMetadata"
-curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G "%SERVICE_HOST%/audit/report?limit=100&category=info&serviceMethod=createDataPackage&serviceMethod=uploadDataPackage&fromTime=2013-10-01
-curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G "%SERVICE_HOST%/audit/report?limit=100&category=info&serviceMethod=updateDataPackage&serviceMethod=uploadDataPackage&fromTime=2013-10-01
+curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G "%SERVICE_HOST%/audit/report?limit=100&category=info&serviceMethod=createDataPackage&serviceMethod=uploadDataPackage&fromTime=2014-10-01
+curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G "%SERVICE_HOST%/audit/report?limit=100&category=info&serviceMethod=updateDataPackage&serviceMethod=uploadDataPackage&fromTime=2014-10-01
