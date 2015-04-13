1fbd7fe921a6ffdd2b34c1e8e61215bb31c50a86
Duane Costa <dcosta@lternet.edu>
2015-04-13 10:00:40 -0700

    Ticket #806  - Improvements to dataserver error handling

--- dataserver/src/edu/lternet/pasta/datapackagemanager/dataserver/DataServerServlet.java
+++ dataserver/src/edu/lternet/pasta/datapackagemanager/dataserver/DataServerServlet.java
@@ -25,11 +25,12 @@
 package edu.lternet.pasta.datapackagemanager.dataserver;
 
 import java.io.File;
-
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
  
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -76,68 +77,85 @@ public class DataServerServlet extends HttpServlet {
 	 * by the Data Package Manager service.
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-    		throws ServletException, IOException {
+    		throws ServletException {
     	String dataToken = request.getParameter("dataToken");
     	String size = request.getParameter("size");
     	String objectName= request.getParameter("objectName");
     	
-    	/*
-    	 * Find out which directory the temporary data files are being
-    	 * placed in by the Data Package Manager
-    	 */
-    	PropertiesConfiguration options = ConfigurationListener.getOptions();
-    	String tmpDir = options.getString("datapackagemanager.tmpDir");
-    	
-    	if (tmpDir == null || tmpDir.equals("")) {
-    		throw new ServletException("datapackagemanager.tmpDir property value was not specified.");
+    	if (
+    		dataToken == null || dataToken.isEmpty() ||
+    		size == null || size.isEmpty() ||
+    		objectName == null || objectName.isEmpty()
+    	   ) {
+    		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
     	}
+    	else {
+    		/*
+    		 * Find out which directory the temporary data files are being
+    		 * placed in by the Data Package Manager
+    		 */
+    		PropertiesConfiguration options = ConfigurationListener.getOptions();
+    		String tmpDir = options.getString("datapackagemanager.tmpDir");
     	
-    	// reads input file from an absolute path
-        String filePath = String.format("%s/%s", tmpDir, dataToken);
-        File downloadFile = new File(filePath);
-        FileInputStream inStream = new FileInputStream(downloadFile);
+    		if (tmpDir == null || tmpDir.equals("")) {
+    			throw new ServletException("datapackagemanager.tmpDir property value was not specified.");
+    		}
+    	
+    		try { 
+    			// reads input file from an absolute path
+    			String filePath = String.format("%s/%s", tmpDir, dataToken);
+    			File downloadFile = new File(filePath);
+    			FileInputStream inStream = new FileInputStream(downloadFile);
          
-        ServletContext context = getServletContext();
+    			ServletContext context = getServletContext();
          
-        // gets MIME type of the file
-        String mimeType = context.getMimeType(filePath);
-        if (mimeType == null) {        
-            // set to binary type if MIME mapping not found
-            mimeType = "application/octet-stream";
-        }
-        logger.debug("MIME type: " + mimeType);
+    			// gets MIME type of the file
+    			String mimeType = context.getMimeType(filePath);
+    			if (mimeType == null) {        
+    				// set to binary type if MIME mapping not found
+    				mimeType = "application/octet-stream";
+    			}
+    			logger.debug("MIME type: " + mimeType);
          
-        // modifies response
-        response.setContentType(mimeType);
-        response.setContentLength(new Integer(size));
+    			// modifies response
+    			response.setContentType(mimeType);
+    			response.setContentLength(new Integer(size));
          
-        // forces download
-        String headerKey = "Content-Disposition";
-        String headerValue = String.format("attachment; filename=\"%s\"", objectName);
-        response.setHeader(headerKey, headerValue);
+    			// forces download
+    			String headerKey = "Content-Disposition";
+    			String headerValue = String.format("attachment; filename=\"%s\"", objectName);
+    			response.setHeader(headerKey, headerValue);
          
-        // obtains response's output stream
-        OutputStream outStream = response.getOutputStream();
+    			// obtains response's output stream
+    			OutputStream outStream = response.getOutputStream();
          
-        byte[] buffer = new byte[4096];
-        int bytesRead = -1;
+    			byte[] buffer = new byte[4096];
+    			int bytesRead = -1;
          
-        while ((bytesRead = inStream.read(buffer)) != -1) {
-            outStream.write(buffer, 0, bytesRead);
-        }
+    			while ((bytesRead = inStream.read(buffer)) != -1) {
+    				outStream.write(buffer, 0, bytesRead);
+    			}
          
-        inStream.close();
-        outStream.close();
+    			inStream.close();
+    			outStream.close();
         
-        /*
-         * Delete the temporary data file after it was downloaded
-         */
-        try {
-        	downloadFile.delete();
-        }
-        catch (SecurityException e) {
-        	logger.warn(String.format("Error deleting temporary data file: %s", e.getMessage()));
-        }
+    			/*
+    			 * Delete the temporary data file after it was downloaded
+    			 */
+    			try {
+    				downloadFile.delete();
+    			}
+    			catch (SecurityException e) {
+    				logger.warn(String.format("Error deleting temporary data file: %s", e.getMessage()));
+    			}
+    		}
+    		catch (FileNotFoundException e) {
+    			response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+    		}
+    		catch (IOException e) {
+    			throw new ServletException(e.getMessage());
+    		}
+    	}
     }
 	
 }
