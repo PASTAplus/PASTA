f70fca558c234c3379c2911661b17ada487de607
Duane Costa <dcosta@lternet.edu>
2015-11-13 09:55:23 -0700

    Ticket #899: Solr indexing logic is indexing only the first set of geographic coverage coordinates

--- common/src/edu/lternet/pasta/common/eml/EMLParser.java
+++ common/src/edu/lternet/pasta/common/eml/EMLParser.java
@@ -68,10 +68,6 @@ public class EMLParser {
   public static final String BEGIN_DATE_PATH = "//eml/dataset/coverage/temporalCoverage/rangeOfDates/beginDate/calendarDate";
   public static final String END_DATE_PATH = "//eml/dataset/coverage/temporalCoverage/rangeOfDates/endDate/calendarDate";
   public static final String GEOGRAPHIC_COVERAGE_PATH = "//eml/dataset/coverage/geographicCoverage";
-  public static final String WEST_PATH = "//eml/dataset/coverage/geographicCoverage/boundingCoordinates/westBoundingCoordinate";
-  public static final String SOUTH_PATH = "//eml/dataset/coverage/geographicCoverage/boundingCoordinates/southBoundingCoordinate";
-  public static final String EAST_PATH = "//eml/dataset/coverage/geographicCoverage/boundingCoordinates/eastBoundingCoordinate";
-  public static final String NORTH_PATH = "//eml/dataset/coverage/geographicCoverage/boundingCoordinates/northBoundingCoordinate";
   public static final String TITLE_PATH = "//dataset/title";
   public static final String ABSTRACT_PATH = "//dataset/abstract";
   public static final String DATA_SOURCE_PATH = "//methods/methodStep/dataSource/distribution/online/url";
@@ -244,34 +240,6 @@ public class EMLParser {
                     this.dataPackage.addBoundingCoordinates(north, south, east, west);
             	}
             }      
-        }
-
-        /*
-         * Parse the spatial bounding coordinates
-         */
-        
-        Node eastNode = xpathapi.selectSingleNode(document, EAST_PATH);
-        if (eastNode != null) {
-          String coord = eastNode.getTextContent().trim();
-          this.dataPackage.setEastBoundingCoordinate(coord);
-        }
-
-        Node westNode = xpathapi.selectSingleNode(document, WEST_PATH);
-        if (westNode != null) {
-          String coord = westNode.getTextContent().trim();
-          this.dataPackage.setWestBoundingCoordinate(coord);
-        }
-
-        Node northNode = xpathapi.selectSingleNode(document, NORTH_PATH);
-        if (northNode != null) {
-          String coord = northNode.getTextContent().trim();
-          this.dataPackage.setNorthBoundingCoordinate(coord);
-        }
-
-        Node southNode = xpathapi.selectSingleNode(document, SOUTH_PATH);
-        if (southNode != null) {
-          String coord = southNode.getTextContent().trim();
-          this.dataPackage.setSouthBoundingCoordinate(coord);
         }
 
         // Parse geographicDescription node
