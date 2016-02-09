0537ff806cc98822b3a2c05de10e580ed8f76cab
Duane Costa <dcosta@lternet.edu>
2016-02-09 09:42:27 -0700

    Modify test script.

--- DataPackageManager/bin/curl_read_metadata.bat
+++ DataPackageManager/bin/curl_read_metadata.bat
@@ -2,11 +2,11 @@
 ECHO DataPackageManager-0.3 test suite for use on local Windows 7 development workstation 
 
 REM SET SERVICE_HOST=http://localhost:8080
-SET SERVICE_HOST=http://pasta.lternet.edu
+SET SERVICE_HOST=http://pasta-s.lternet.edu
 SET AUTH_TOKEN_UCARROLL=dWlkPXVjYXJyb2xsLG89TFRFUixkYz1lY29pbmZvcm1hdGljcyxkYz1vcmcqaHR0cHM6Ly9wYXN0YS5sdGVybmV0LmVkdS9hdXRoZW50aWNhdGlvbioyMDAwMDAwMDAwKmF1dGhlbnRpY2F0ZWQ=
 SET AUTH_TOKEN_DCOSTA=dWlkPWRjb3N0YSxvPUxURVIsZGM9ZWNvaW5mb3JtYXRpY3MsZGM9b3JnKmh0dHBzOi8vcGFzdGEubHRlcm5ldC5lZHUvYXV0aGVudGljYXRpb24qMjAwMDAwMDAwMCphdXRoZW50aWNhdGVk
-SET SCOPE=knb-lter-vcr
-SET IDENTIFIER=1
-SET REVISION=newest
+SET SCOPE=knb-lter-arc
+SET IDENTIFIER=1424
+SET REVISION=5
 
 curl -G %SERVICE_HOST%/package/metadata/eml/%SCOPE%/%IDENTIFIER%/%REVISION%
