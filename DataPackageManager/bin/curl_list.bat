7631721dc045789d3741d000f09af6885f574837
Duane Costa <dcosta@lternet.edu>
2016-06-02 14:41:30 -0700

    Update to test script that calls a number of web service methods for listing PASTA resources.

--- DataPackageManager/bin/curl_list.bat
+++ DataPackageManager/bin/curl_list.bat
@@ -1,23 +1,19 @@
 @ECHO OFF
-ECHO DataPackageManager-0.1 test suite for use on local Windows 7 development workstation 
 
-SET SERVICE_HOST=http://localhost:8080
-SET AUTH_TOKEN_UCARROLL=dWlkPXVjYXJyb2xsLG89TFRFUixkYz1lY29pbmZvcm1hdGljcyxkYz1vcmcqaHR0cHM6Ly9wYXN0YS5sdGVybmV0LmVkdS9hdXRoZW50aWNhdGlvbioyMDAwMDAwMDAwKmF1dGhlbnRpY2F0ZWQ=
-SET AUTH_TOKEN_DCOSTA=dWlkPWRjb3N0YSxvPUxURVIsZGM9ZWNvaW5mb3JtYXRpY3MsZGM9b3JnKmh0dHBzOi8vcGFzdGEubHRlcm5ldC5lZHUvYXV0aGVudGljYXRpb24qMjAwMDAwMDAwMCphdXRoZW50aWNhdGVk
-SET SCOPE=knb-lter-nin
+REM SET SERVICE_HOST=http://localhost:8888
+REM SET SERVICE_HOST=https://pasta-d.lternet.edu
+REM SET SERVICE_HOST=https://pasta-s.lternet.edu
+SET SERVICE_HOST=https://pasta.lternet.edu
+
+SET SCOPE=knb-lter-jrn
 SET BOGUS_SCOPE=xxx
-SET IDENTIFIER=1
-SET ENTITY_ID=NoneSuchBugCount
+SET IDENTIFIER=210001001
+SET REVISION=41
 
-ECHO DP-5: DP-5 List Data Package Scopes, Identifiers, Revisions, or Data Entities
-curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G %SERVICE_HOST%/package/eml
-curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G %SERVICE_HOST%/package/eml/%SCOPE%
-curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G %SERVICE_HOST%/package/eml/%SCOPE%/%IDENTIFIER%
-curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G %SERVICE_HOST%/package/data/eml/%SCOPE%/%IDENTIFIER%/1
-curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G %SERVICE_HOST%/package/eml/%SCOPE%/%IDENTIFIER%
-curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G %SERVICE_HOST%/package/eml/%SCOPE%/%IDENTIFIER%?filter=oldest
-curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G %SERVICE_HOST%/package/eml/%SCOPE%/%IDENTIFIER%?filter=newest
-curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G %SERVICE_HOST%/package/eml/%BOGUS_SCOPE%
-curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G %SERVICE_HOST%/package/eml/%BOGUS_SCOPE%/%IDENTIFIER%
-curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G %SERVICE_HOST%/package/eml/%BOGUS_SCOPE%/%IDENTIFIER%?filter=oldest
-curl -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G %SERVICE_HOST%/package/eml/%BOGUS_SCOPE%/%IDENTIFIER%?filter=newest
+curl -X GET "%SERVICE_HOST%/package/eml"
+curl -X GET "%SERVICE_HOST%/package/eml/%SCOPE%"
+curl -X GET "%SERVICE_HOST%/package/eml/%SCOPE%/%IDENTIFIER%"
+curl -X GET "%SERVICE_HOST%/package/data/eml/%SCOPE%/%IDENTIFIER%/%REVISION%
+curl -X GET "%SERVICE_HOST%/package/eml/%SCOPE%/%IDENTIFIER%"
+curl -X GET "%SERVICE_HOST%/package/eml/%SCOPE%/%IDENTIFIER%?filter=oldest"
+curl -X GET "%SERVICE_HOST%/package/eml/%SCOPE%/%IDENTIFIER%?filter=newest"
