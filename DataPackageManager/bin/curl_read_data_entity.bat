f549cf1434dbabbd432ca69a1e6a3d12c5163cf4
Duane Costa <dcosta@lternet.edu>
2016-08-23 14:10:23 -0700

    Modification to test script.

--- DataPackageManager/bin/curl_read_data_entity.bat
+++ DataPackageManager/bin/curl_read_data_entity.bat
@@ -1,13 +1,10 @@
 @ECHO OFF
 ECHO DataPackageManager-0.1 test suite for use on local Windows 7 development workstation 
 
-SET SERVICE_HOST=http://localhost:8080
-SET AUTH_TOKEN_UCARROLL=dWlkPXVjYXJyb2xsLG89TFRFUixkYz1lY29pbmZvcm1hdGljcyxkYz1vcmcqaHR0cHM6Ly9wYXN0YS5sdGVybmV0LmVkdS9hdXRoZW50aWNhdGlvbioyMDAwMDAwMDAwKmF1dGhlbnRpY2F0ZWQ=
-SET AUTH_TOKEN_DCOSTA=dWlkPWRjb3N0YSxvPUxURVIsZGM9ZWNvaW5mb3JtYXRpY3MsZGM9b3JnKmh0dHBzOi8vcGFzdGEubHRlcm5ldC5lZHUvYXV0aGVudGljYXRpb24qMjAwMDAwMDAwMCphdXRoZW50aWNhdGVk
+SET SERVICE_HOST=http://localhost:8888
 SET SCOPE=knb-lter-nin
 SET IDENTIFIER=1
 SET REVISION=1
 SET ENTITY_ID=67e99349d1666e6f4955e9dda42c3cc2
 
-ECHO DP-?: Read a Data Entity
-curl -L -i -b auth-token=%AUTH_TOKEN_UCARROLL% -G %SERVICE_HOST%/package/data/eml/%SCOPE%/%IDENTIFIER%/%REVISION%/%ENTITY_ID%
+curl -i -G %SERVICE_HOST%/package/data/eml/%SCOPE%/%IDENTIFIER%/%REVISION%/%ENTITY_ID%
