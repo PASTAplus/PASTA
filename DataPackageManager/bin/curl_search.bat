a12d14931c5cac80d4cc01c29a7cb94455e7927c
Duane Costa <dcosta@lternet.edu>
2016-08-23 10:29:37 -0700

    Modifications to test script for search.

--- DataPackageManager/bin/curl_search.bat
+++ DataPackageManager/bin/curl_search.bat
@@ -6,4 +6,5 @@ REM SET SERVICE_HOST=http://pasta-s.lternet.edu:8080
 SET SERVICE_HOST=http://pasta.lternet.edu
 
 REM curl -X GET "%SERVICE_HOST%/package/search/eml?defType=edismax&q=%22soil%20temperature%22&fq=-scope:ecotrends&fq=-scope:lter-landsat*&fl=packageid&sort=score,desc&sort=packageid,asc&debug=false&start=0&rows=1000"
-curl -X GET "%SERVICE_HOST%/package/search/eml?defType=edismax&q=soil&q=temperature&fq=-scope:ecotrends&fq=-scope:lter-landsat*&fl=packageid&sort=score,desc&sort=packageid,asc&debug=false&start=0&rows=10"
+REM curl -X GET "%SERVICE_HOST%/package/search/eml?defType=edismax&q=soil&q=temperature&fq=-scope:ecotrends&fq=-scope:lter-landsat*&fl=packageid&sort=score,desc&sort=packageid,asc&debug=false&start=0&rows=10"
+curl -X GET "%SERVICE_HOST%/package/search/eml?defType=edismax&q=water+balance&fl=*&sort=score,desc&sort=packageid,asc&debug=false&start=0&rows=10"
