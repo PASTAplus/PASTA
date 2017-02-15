91c9537571f6ec47530e70a55a2ade30b048c13c
Duane Costa <dcosta@lternet.edu>
2017-02-15 15:13:19 -0700

    Refactor the script to pass in the date on the command line instead of it being hard-coded.

--- DataPackageManager/bin/curl_listDataPackageChanges.bat
+++ DataPackageManager/bin/curl_listDataPackageChanges.bat
@@ -1,21 +1,22 @@
 @ECHO OFF
 echo.
-echo Reservations for: localhost
+echo Data package changes for: localhost
 echo.
+SET FROM_DATE=%1
 SET SERVICE_HOST=http://localhost:8888
-curl -X GET "%SERVICE_HOST%/package/changes/eml?fromDate=2017-02-06T17:00:00"
+curl -X GET "%SERVICE_HOST%/package/changes/eml?fromDate=%FROM_DATE%"
 echo.
-echo Reservations for: pasta-d
+echo Data package changes for: pasta-d
 echo.
 SET SERVICE_HOST=http://pasta-d.lternet.edu
-REM curl -X GET "%SERVICE_HOST%/package/changes/eml?fromDate=2017-01-01"
+REM curl -X GET "%SERVICE_HOST%/package/changes/eml?fromDate=%FROM_DATE%"
 echo.
-echo Reservations for: pasta-s
+echo Data package changes for: pasta-s
 echo.
 SET SERVICE_HOST=http://pasta-s.lternet.edu
-REM curl -X GET "%SERVICE_HOST%/package/changes/eml?fromDate=2017-01-01"
+REM curl -X GET "%SERVICE_HOST%/package/changes/eml?fromDate=%FROM_DATE%"
 echo.
-echo Reservations for: pasta
+echo Data package changes for: pasta
 echo.
 SET SERVICE_HOST=http://pasta.lternet.edu
-REM curl -X GET "%SERVICE_HOST%/package/changes/eml?fromDate=2017-01-01"
+REM curl -X GET "%SERVICE_HOST%/package/changes/eml?fromDate=%FROM_DATE%"
