1002ad262804f366bed50602f1aae409aad1e7a8
Duane Costa <dcosta@lternet.edu>
2017-03-06 13:16:14 -0700

    Changes to test script.

---
+++ DataPackageManager/bin/curl_listRecentChanges.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+SET FROM_DATE=%1
+echo.
+echo Data package changes for: localhost
+echo.
+SET SERVICE_HOST=http://localhost:8888
+curl -X GET "%SERVICE_HOST%/package/changes/eml?fromDate=%FROM_DATE%"
+echo.
+echo Data package changes for: pasta-d
+echo.
+SET SERVICE_HOST=http://pasta-d.lternet.edu
+curl -X GET "%SERVICE_HOST%/package/changes/eml?fromDate=%FROM_DATE%"
+echo.
+echo Data package changes for: pasta-s
+echo.
+SET SERVICE_HOST=http://pasta-s.lternet.edu
+curl -X GET "%SERVICE_HOST%/package/changes/eml?fromDate=%FROM_DATE%"
+echo.
+REM echo Data package changes for: pasta
+REM echo.
+REM SET SERVICE_HOST=http://pasta.lternet.edu
+REM curl -X GET "%SERVICE_HOST%/package/changes/eml?fromDate=%FROM_DATE%"
