a0ca09bde50c523708f8b986c5d286edf53f3941
Duane Costa <dcosta@lternet.edu>
2017-12-21 11:44:12 -0700

    Update to test script.

--- DataPackageManager/bin/curl_readResourceMetadata.bat
+++ DataPackageManager/bin/curl_readResourceMetadata.bat
@@ -1,14 +1,19 @@
 @ECHO OFF
 
-SET SERVICE_HOST=http://localhost:8888
-REM SET SERVICE_HOST=http://package-d.lternet.edu:8080
-REM SET SERVICE_HOST=http://package-s.lternet.edu:8080
-REM SET SERVICE_HOST=http://pasta.lternet.edu
+REM SET SERVICE_HOST=http://localhost:8888
+REM SET SERVICE_HOST=https://pasta-d.lternet.edu
+REM SET SERVICE_HOST=https://pasta-s.lternet.edu
+SET SERVICE_HOST=https://pasta.lternet.edu
 
 SET SCOPE=knb-lter-nin
 SET IDENTIFIER=1
 SET REVISION=1
-SET ENTITY_ID=67e99349d1666e6f4955e9dda42c3cc2
+
+REM Use this entity_id on development where knb-lter-nin.1.1 was added more recently
+REM SET ENTITY_ID=67e99349d1666e6f4955e9dda42c3cc2
+
+REM Use this entity_id on production where knb-lter-nin.1.1 was add early in PASTA's history
+SET ENTITY_ID=DailyWaterSample-NIN-LTER-1978-1992
 
 curl -X GET "%SERVICE_HOST%/package/rmd/eml/%SCOPE%/%IDENTIFIER%/%REVISION%"
 curl -X GET "%SERVICE_HOST%/package/metadata/rmd/eml/%SCOPE%/%IDENTIFIER%/%REVISION%"
