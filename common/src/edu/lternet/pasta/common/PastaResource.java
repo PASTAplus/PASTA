bdfa76f12f7f6b33a74c27677970be159d7cd871
jide <jride@wisc.edu>
2019-08-16 15:14:52 -0500

    Refactor getResourceAcl method in datapackagemanager.DataPackageRegistry class to identify correct EML schema version when creating the ACL fragment

--- common/src/edu/lternet/pasta/common/PastaResource.java
+++ common/src/edu/lternet/pasta/common/PastaResource.java
@@ -20,6 +20,8 @@
 
 package edu.lternet.pasta.common;
 
+import com.sun.jersey.api.core.ScanningResourceConfig;
+
 /**
  * @author servilla
  * @since Nov 12, 2012
@@ -32,8 +34,6 @@ public class PastaResource {
 	/*
 	 * Class variables
 	 */
-	
-	private static final String PACKAGE_PATTERN = "knb-lter-";
 
 	/*
 	 * Instance variables
@@ -47,42 +47,102 @@ public class PastaResource {
 	 * Class methods
 	 */
 
-	/*
-	 * Instance methods
-	 */
-	
 	/**
 	 * Returns the canonical EML package identifier from the object PASTA resource
-	 * identifier.  Note: this method assumes that the scope identifier part of
-	 * the package identifier begins with the substring "knb-lter".
+	 * identifier.  Note: this method assumes that the resourceId contains one of the
+	 * following substrings:
+	 *    /package/data/eml/
+	 *    /package/report/eml/
+	 *    /package/eml/
+	 *    /package/metadata/eml/
+	 * If not, it throws an IllegalArgumentException.
 	 * 
 	 * @return The canonical EML package identifier
 	 */
-	static public String getPackageId(String resourceId) {
+	static public String getPackageId(String resourceId)
+		throws IllegalArgumentException {
 		
 		String packageId = null;
-		String scope = null;
-		String identifier = null;
-		String revision = null;
-		
-		String[] pathParts = resourceId.split("/");
-		String part = null;
-		
-		for (int i = 0; i < pathParts.length; i++) {
-			
-			if (pathParts[i].contains(PACKAGE_PATTERN)) {
-				scope = pathParts[i];
-				identifier = pathParts[i+1];
-				revision = pathParts[i+2];
-				packageId = scope + "." + identifier + "." + revision;
+
+		String[] canonicalSubstrings = { "/package/data/eml/", "/package/report/eml/", "/package/eml/", "/package/metadata/eml/" };
+		int index = -1;
+
+		for (int i = 0; i < canonicalSubstrings.length; i++) {
+			index = resourceId.indexOf(canonicalSubstrings[i]);
+			if (index > -1) {
+				String tail = resourceId.substring(index + canonicalSubstrings[i].length());
+				if (tail != null) {
+					String[] pathParts = tail.split("/");
+					String scope = pathParts[0];
+					String identifier = pathParts[1];
+					String revision = pathParts[2];
+					packageId = scope + "." + identifier + "." + revision;
+				}
 				break;
 			}
-			
 		}
-		
+
+		if (index == -1 || packageId == null) {
+			throw new IllegalArgumentException("Bad resource id: " + resourceId);
+		}
+
 		return packageId;
 		
 	}
+
+
+	/**
+	 * Returns the canonical EML metadata resourceId corresponding to the given resourceId.
+	 * For example, given resourceId
+	 *     http://localhost:8088/package/data/eml/knb-lter-nin/1/1/67e99349d1666e6f4955e9dda42c3cc2
+	 * returns
+	 *     http://localhost:8088/package/metadata/eml/knb-lter-nin/1/1
+	 * Note: this method assumes that the given resourceId contains one of the
+	 * following substrings:
+	 *    /package/data/eml/
+	 *    /package/report/eml/
+	 *    /package/eml/
+	 *    /package/metadata/eml/
+	 * If not, it throws an IllegalArgumentException.
+	 *
+	 * @return The EML metadata resourceId
+	 */
+	static public String getMetadataResourceId(String resourceId)
+			throws IllegalArgumentException {
+
+		String metadataResourceId = null;
+
+		String[] canonicalSubstrings = { "/package/data/eml/", "/package/report/eml/", "/package/eml/", "/package/metadata/eml/" };
+		int index = -1;
+
+		for (int i = 0; i < canonicalSubstrings.length; i++) {
+			index = resourceId.indexOf(canonicalSubstrings[i]);
+			if (index > -1) {
+				String head = resourceId.substring(0, index);
+				String tail = resourceId.substring(index + canonicalSubstrings[i].length());
+				if (head != null && tail != null) {
+					String[] pathParts = tail.split("/");
+					String scope = pathParts[0];
+					String identifier = pathParts[1];
+					String revision = pathParts[2];
+					String packageId = scope + "/" + identifier + "/" + revision;
+					metadataResourceId = head + "/package/metadata/eml/" + packageId;
+				}
+				break;
+			}
+		}
+
+		if (index == -1 || metadataResourceId == null) {
+			throw new IllegalArgumentException();
+		}
+
+		return metadataResourceId;
+
+	}
+
+	/*
+	 * Instance methods
+	 */
 
 	/**
 	 * @param args
