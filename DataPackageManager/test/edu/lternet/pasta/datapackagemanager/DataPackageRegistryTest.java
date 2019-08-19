f017688e789844912d84b88e529f7a5194c19f82
jide <jride@wisc.edu>
2019-08-19 14:15:55 -0500

    Refactor getResourceAcl method in datapackagemanager.DataPackageRegistry class to identify correct EML schema version when creating the ACL fragment

--- DataPackageManager/test/edu/lternet/pasta/datapackagemanager/DataPackageRegistryTest.java
+++ DataPackageManager/test/edu/lternet/pasta/datapackagemanager/DataPackageRegistryTest.java
@@ -24,18 +24,77 @@
 
 package edu.lternet.pasta.datapackagemanager;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
+import edu.lternet.pasta.common.PastaResource;
 import org.junit.Test;
 
 
 public class DataPackageRegistryTest {
-
-  /**
+    /**
    * Test initialization of the test suite
    */
-  @Test public void testInit() {
+    @Test public void testInit() {
     assertTrue(1 == 1);
   }
 
+    @Test public void testGetPackageId() {
+        String resourceId, packageId;
+        String EXPECTED = "knb-lter-nin.1.1";
+
+        resourceId = "http://pasta.lternet.edu/package/data/eml/knb-lter-nin/1/1/67e99349d1666e6f4955e9dda42c3cc2";
+        packageId = PastaResource.getPackageId(resourceId);
+        assertEquals(packageId, EXPECTED);
+
+        resourceId = "http://pasta.lternet.edu/package/report/eml/knb-lter-nin/1/1/";
+        packageId = PastaResource.getPackageId(resourceId);
+        assertEquals(packageId, EXPECTED);
+
+        resourceId = "http://pasta.lternet.edu/package/eml/knb-lter-nin/1/1/";
+        packageId = PastaResource.getPackageId(resourceId);
+        assertEquals(packageId, EXPECTED);
+
+        resourceId = "http://pasta.lternet.edu/package/metadata/eml/knb-lter-nin/1/1/";
+        packageId = PastaResource.getPackageId(resourceId);
+        assertEquals(packageId, EXPECTED);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetPackageIdException() {
+        String resourceId, packageId;
+
+        resourceId = "http://pasta.lternet.edu/package/FOOBAR/eml/knb-lter-nin/1/1/";
+        packageId = PastaResource.getPackageId(resourceId);
+        assertTrue(false);  // shouldn't reach this line...
+    };
+
+    @Test public void testGetMetadataResourceId() {
+        String resourceId, metadataResourceId;
+        String EXPECTED = "http://pasta.lternet.edu/package/metadata/eml/knb-lter-nin/1/1";
+
+        resourceId = "http://pasta.lternet.edu/package/data/eml/knb-lter-nin/1/1/67e99349d1666e6f4955e9dda42c3cc2";
+        metadataResourceId = PastaResource.getMetadataResourceId(resourceId);
+        assertEquals(metadataResourceId, EXPECTED);
+
+        resourceId = "http://pasta.lternet.edu/package/report/eml/knb-lter-nin/1/1/";
+        metadataResourceId = PastaResource.getMetadataResourceId(resourceId);
+        assertEquals(metadataResourceId, EXPECTED);
+
+        resourceId = "http://pasta.lternet.edu/package/eml/knb-lter-nin/1/1/";
+        metadataResourceId = PastaResource.getMetadataResourceId(resourceId);
+        assertEquals(metadataResourceId, EXPECTED);
+
+        resourceId = "http://pasta.lternet.edu/package/metadata/eml/knb-lter-nin/1/1/";
+        metadataResourceId = PastaResource.getMetadataResourceId(resourceId);
+        assertEquals(metadataResourceId, EXPECTED);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testGetMetadataResourceIdException() {
+        String resourceId, metadataResourceId;
+
+        resourceId = "http://pasta.lternet.edu/package/FOOBAR/eml/knb-lter-nin/1/1/";
+        metadataResourceId = PastaResource.getMetadataResourceId(resourceId);
+        assertTrue(false);  // shouldn't reach this line...
+    };
 }
