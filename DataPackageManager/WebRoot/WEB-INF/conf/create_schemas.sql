aae83b20492c56ad6467b2ffbc815135c2b9dd08
Mark Servilla <mark.servilla@gmail.com>
2023-02-07 20:12:22 -0700

    Fix create schema syntax error again doh

--- DataPackageManager/WebRoot/WEB-INF/conf/create_schemas.sql
+++ DataPackageManager/WebRoot/WEB-INF/conf/create_schemas.sql
@@ -75,7 +75,7 @@ CREATE TABLE datapackagemanager.journal_citation (
   DATE_CREATED    TIMESTAMP    NOT NULL,              -- date that the identifier was reserved
   JOURNAL_TITLE   TEXT,                               -- name of the journal
   PACKAGE_ID      VARCHAR(100) NOT NULL,               -- packageId of source data package in PASTA
-  RELATION_TYPE datapackagemanager.relation_type NOT NULL, -- type of relation between data package subject and citation object
+  RELATION_TYPE datapackagemanager.relation_type NOT NULL -- type of relation between data package subject and citation object
 );
 
 
