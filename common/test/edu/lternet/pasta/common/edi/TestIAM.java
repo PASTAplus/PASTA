3a921073b9b5bdaba7de8fd14f09a6bdd60d5876
Mark Servilla <mark.servilla@gmail.com>
2025-07-23 01:44:37 +0000

    Add IAM unit test

---
+++ common/test/edu/lternet/pasta/common/edi/TestIAM.java
@@ -0,0 +1,60 @@
+/*
+ *
+ * $Date$
+ * $Author$
+ * $Revision$
+ *
+ * Copyright 2010 the University of New Mexico.
+ *
+ * This work was supported by National Science Foundation Cooperative
+ * Agreements #DEB-0832652 and #DEB-0936498.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ */
+
+package edu.lternet.pasta.common.edi;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestIAM {
+    private IAM iam;
+    private String ediToken;
+    private String resourceId;
+    private String permission;
+    private String baseUrl = "https://localhost:5443";
+
+    @Before
+    public void setUp() {
+        ediToken = "eyJhbGciOiJFUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJFREktMzEyZTkwZjI5ZmJlNGUzZGE3MWY1MjFlZmJmMWRlMDYiLCJjbiI6IkVESSIsImVtYWlsIjpudWxsLCJwcmluY2lwYWxzIjpbIkVESS1iMjc1N2ZlZTEyNjM0Y2NjYTQwZDJkNjg5ZjVjMDU0MyIsIkVESS1kM2ZjYTk3NjdhYzU0YzIyOTYyODk2YjAxYTFjMDFiZCJdLCJpc0VtYWlsRW5hYmxlZCI6ZmFsc2UsImlzRW1haWxWZXJpZmllZCI6ZmFsc2UsImlkZW50aXR5SWQiOjMsImlkcE5hbWUiOiJsZGFwIiwiaWRwVWlkIjoidWlkPUVESSxvPUVESSxkYz1lZGlyZXBvc2l0b3J5LGRjPW9yZyIsImlkcENuYW1lIjoiRURJIiwiaXNzIjoiaHR0cHM6Ly9hdXRoLmVkaXJlcG9zaXRvcnkub3JnIiwiaGQiOiJlZGlyZXBvc2l0b3J5Lm9yZyIsImlhdCI6MTc1MjUwNzY5MiwibmJmIjoxNzUyNTA3NjkyLCJleHAiOjE3NTI1MzY0OTJ9.syUA6NedZvSSjYakGWtq5qo4U1H8LzX-R7w0MC5p2fZE8usCqrgu-ig0kLnrYRscBQWrOcwE8E7bbp6yNPTqbQ";
+    }
+
+    @Test
+    public void testIAMConstructor() {
+        iam = new IAM("https", "localhost", 5443, ediToken);
+        assert(iam.getBaseUrl().equals(baseUrl));
+    }
+
+    @Test
+    public void testPing() {
+        iam = new IAM("https", "localhost", 5443, ediToken);
+        String response;
+        try {
+            response = iam.ping();
+            assert response.equals("pong");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            assert false;
+        }
+    }
+}
