0f61bd5245c3d6ec1280b064b2b6e50c9267e3ce
Mark Servilla <mark.servilla@gmail.com>
2025-08-19 18:34:05 +0000

    Add new ForbiddenException class

---
+++ common/src/edu/lternet/pasta/common/security/access/ForbiddenException.java
@@ -0,0 +1,40 @@
+/*
+ * $Date$
+ * $Author$
+ * $Revision$
+ *
+ * Copyright 2010 the University of New Mexico.
+ *
+ * This work was supported by National Science Foundation Cooperative Agreements
+ * #DEB-0832652 and #DEB-0936498.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package edu.lternet.pasta.common.security.access;
+
+import edu.lternet.pasta.common.UserErrorException;
+
+/**
+ * Used to indicate that an attempted operation is forbidden.
+ */
+public class ForbiddenException extends UserErrorException {
+
+    private static final long serialVersionUID = 1L;
+
+    public ForbiddenException(String message) {
+        super(message);
+    }
+
+    public ForbiddenException(String msg, Throwable cause) {
+        super(msg, cause);
+    }
+
+}
